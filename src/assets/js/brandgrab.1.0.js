b4d9f0f4c2e74bc1a67ebfbb46ec67b8d5f0e799
Colin <colinkeany@github.com>
2023-09-04 18:58:19 -0400

    account for other svg tag options

--- src/assets/js/brandgrab.1.0.js
+++ src/assets/js/brandgrab.1.0.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-	// Function to set the background and font color based on the selected color option.
 	function setColor() {
 		var selectedOption = document.querySelector('input[name="colorOption"]:checked');
 		var bgColor = selectedOption.getAttribute('data-bg-color');
@@ -12,9 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 
 		var logoPaths = document.querySelectorAll('.logo path');
-		logoPaths.forEach(function(path) {
-			path.style.fill = fontColor;
-		});
+		var logoCircles = document.querySelectorAll('.logo circle');
+		var logoRects = document.querySelectorAll('.logo rect');
+		var logoEllipses = document.querySelectorAll('.logo ellipse');
+		var logoPolygons = document.querySelectorAll('.logo polygon');
+
+		function applyFill(elements, color) {
+			elements.forEach(function(element) {
+				// For SVG elements with direct attributes
+				if (element.tagName === 'circle' || element.tagName === 'path' || element.tagName === 'rect') {
+					element.setAttribute('fill', color);
+				}
+				// For others, use CSS
+				else {
+					element.style.fill = color;
+				}
+			});
+		}
+
+		applyFill(logoPaths, fontColor);
+		applyFill(logoCircles, fontColor);
+		applyFill(logoRects, fontColor);
+		applyFill(logoEllipses, fontColor);
+		applyFill(logoPolygons, fontColor);
 	}
 
 	var colorOptions = document.querySelectorAll('input[name="colorOption"]');
@@ -137,111 +156,111 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Simple function to replace placeholders with actual data
 	function render(template, data) {
-	    return data.map((item, index) => {
-	        let result = template;
+		return data.map((item, index) => {
+			let result = template;
 
-	        // Construct the class list
-	        let classList = "logo";
-	        if (item.default) {
-	            classList += " currentSVG";
-	        }
+			// Construct the class list
+			let classList = "logo";
+			if (item.default) {
+				classList += " currentSVG";
+			}
 
-	        // Add the incremental ID
-	        let id = "logo" + (index + 1);
-	        
-	        // Insert the class and ID into the SVG inside logoSVG
-	        item.svgClassID = item.logoSVG.replace('<svg', `<svg id="${id}" class="${classList}"`);
-	        
-	        for (let key in item) {
-	            result = result.replace(new RegExp(`{{${key}}}`, 'g'), item[key]);
-	        }
+			// Add the incremental ID
+			let id = "logo" + (index + 1);
+			
+			// Insert the class and ID into the SVG inside logoSVG
+			item.svgClassID = item.logoSVG.replace('<svg', `<svg id="${id}" class="${classList}"`);
 
-	        return result;
-	    });
+			for (let key in item) {
+				result = result.replace(new RegExp(`{{${key}}}`, 'g'), item[key]);
+			}
+
+			return result;
+		});
 	}
 
 	// Get the template content
 	var template = document.getElementById('svgTemplate').textContent;
 
 	// Render content
-    var renderedItems = render(template, data);
-    var contentDiv = document.getElementById('renderedContent');
-    renderedItems.forEach(item => {
-        contentDiv.innerHTML += item;
-    });
+	var renderedItems = render(template, data);
+	var contentDiv = document.getElementById('renderedContent');
+	renderedItems.forEach(item => {
+		contentDiv.innerHTML += item;
+	});
 
-    // Event listeners for width and height inputs
-    document.getElementById('svgWidthInput').addEventListener('input', updateSVGDimensions);
-    document.getElementById('svgHeightInput').addEventListener('input', updateSVGDimensions);
+	// Event listeners for width and height inputs
+	document.getElementById('svgWidthInput').addEventListener('input', updateSVGDimensions);
+	document.getElementById('svgHeightInput').addEventListener('input', updateSVGDimensions);
 
-    // Initialize the input fields with the currentSVG dimensions
-    initializeInputsWithCurrentSVGDimensions();
+	// Initialize the input fields with the currentSVG dimensions
+	initializeInputsWithCurrentSVGDimensions();
 
-    const radios = document.querySelectorAll('input[name="logoSelector"]');
-    const svgs = document.querySelectorAll('.logo');
+	const radios = document.querySelectorAll('input[name="logoSelector"]');
+	const svgs = document.querySelectorAll('.logo');
 
-    radios.forEach(radio => {
-        radio.addEventListener('change', function() {
-            // Remove the 'currentSVG' class from all SVGs
-            svgs.forEach(svg => {
-                svg.classList.remove('currentSVG');
-            });
+	radios.forEach(radio => {
+		radio.addEventListener('change', function() {
+			// Remove the 'currentSVG' class from all SVGs
+			svgs.forEach(svg => {
+				svg.classList.remove('currentSVG');
+			});
 
-            // Add the 'currentSVG' class to the associated SVG
-            const svgId = this.getAttribute('data-svg-id');
-            const associatedSvg = document.getElementById(svgId);
-            if (associatedSvg) {
-                associatedSvg.classList.add('currentSVG');
+			// Add the 'currentSVG' class to the associated SVG
+			const svgId = this.getAttribute('data-svg-id');
+			const associatedSvg = document.getElementById(svgId);
+			if (associatedSvg) {
+				associatedSvg.classList.add('currentSVG');
 
-                // Update the input fields to reflect the dimensions of the newly selected SVG
-                document.getElementById('svgWidthInput').value = associatedSvg.getAttribute('width');
-                document.getElementById('svgHeightInput').value = associatedSvg.getAttribute('height');
-            }
-        });
-    });
+				// Update the input fields to reflect the dimensions of the newly selected SVG
+				document.getElementById('svgWidthInput').value = associatedSvg.getAttribute('width');
+				document.getElementById('svgHeightInput').value = associatedSvg.getAttribute('height');
+			}
+		});
+	});
 });
 
 function initializeInputsWithCurrentSVGDimensions() {
-    let svgElement = document.querySelector('.currentSVG');
+	let svgElement = document.querySelector('.currentSVG');
 
-    // Get the current SVG dimensions
-    let svgWidth = svgElement.getAttribute('width');
-    let svgHeight = svgElement.getAttribute('height');
+	// Get the current SVG dimensions
+	let svgWidth = svgElement.getAttribute('width');
+	let svgHeight = svgElement.getAttribute('height');
 
-    // Populate the width and height input fields
-    document.getElementById('svgWidthInput').value = svgWidth;
-    document.getElementById('svgHeightInput').value = svgHeight;
+	// Populate the width and height input fields
+	document.getElementById('svgWidthInput').value = svgWidth;
+	document.getElementById('svgHeightInput').value = svgHeight;
 }
 
 function updateSVGDimensions() {
-    let svgElement = document.querySelector('.currentSVG');
+	let svgElement = document.querySelector('.currentSVG');
 
-    // Calculate the original width-to-height ratio
-    let svgWidth = parseFloat(svgElement.getAttribute('width'));
-    let svgHeight = parseFloat(svgElement.getAttribute('height'));
-    let originalRatio = svgWidth / svgHeight;
+	// Calculate the original width-to-height ratio
+	let svgWidth = parseFloat(svgElement.getAttribute('width'));
+	let svgHeight = parseFloat(svgElement.getAttribute('height'));
+	let originalRatio = svgWidth / svgHeight;
 
-    // Get the input values
-    let newWidth = parseFloat(document.getElementById('svgWidthInput').value);
-    let newHeight = parseFloat(document.getElementById('svgHeightInput').value);
+	// Get the input values
+	let newWidth = parseFloat(document.getElementById('svgWidthInput').value);
+	let newHeight = parseFloat(document.getElementById('svgHeightInput').value);
 
-    if (!newWidth || isNaN(newWidth) || !newHeight || isNaN(newHeight)) {
-        document.getElementById('svgWidthInput').value = svgWidth;
-        document.getElementById('svgHeightInput').value = svgHeight;
-        return; // exit the function if either input is cleared
-    }
+	if (!newWidth || isNaN(newWidth) || !newHeight || isNaN(newHeight)) {
+		document.getElementById('svgWidthInput').value = svgWidth;
+		document.getElementById('svgHeightInput').value = svgHeight;
+		return; // exit the function if either input is cleared
+	}
 
-    if (this.id === 'svgWidthInput') {
-        // Width was changed, so adjust height
-        newHeight = newWidth / originalRatio;
-        document.getElementById('svgHeightInput').value = newHeight.toFixed(2);
-    } else {
-        // Height was changed, so adjust width
-        newWidth = newHeight * originalRatio;
-        document.getElementById('svgWidthInput').value = newWidth.toFixed(2);
-    }
+	if (this.id === 'svgWidthInput') {
+		// Width was changed, so adjust height
+		newHeight = newWidth / originalRatio;
+		document.getElementById('svgHeightInput').value = newHeight.toFixed(2);
+	} else {
+		// Height was changed, so adjust width
+		newWidth = newHeight * originalRatio;
+		document.getElementById('svgWidthInput').value = newWidth.toFixed(2);
+	}
 
-    // Update SVG dimensions
-    svgElement.setAttribute('width', newWidth);
-    svgElement.setAttribute('height', newHeight);
+	// Update SVG dimensions
+	svgElement.setAttribute('width', newWidth);
+	svgElement.setAttribute('height', newHeight);
 }
